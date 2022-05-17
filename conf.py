aec35f3e00c87c53b0f60a240757e3f8818b4913
57Yadier <103086948+57Yadier@users.noreply.github.com>
2022-05-17 10:14:52 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 api_hash = "a5b9168c22453fed0bb5503e48074035"
-api_id = 1850218220
+api_id = 9517961
 bot_token = "5337490295:AAG3gb0zp_u-z5hJohbNoOo3pLORu0zSBVA"
 authorized_users = []
 admin_user = '@Fatherlandandlife'
